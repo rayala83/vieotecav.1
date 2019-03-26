df76f5e42f118382be8c1f8c1e29cf507ffac196
rayala34 <ragieci@gmail.com>
2019-03-26 12:48:07 -0300

    se agrega funcionalidad para  movimieno de slides segun tiempos de videos

--- desarrollo_memoria_conferencias/aplicacion_conferencia/app/presentacion_de_conferencias/views.py
+++ desarrollo_memoria_conferencias/aplicacion_conferencia/app/presentacion_de_conferencias/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
 from app.presentacion_de_conferencias.models import Diapositiva, slides_ppt
 from app.videos_de_conferencias.models import Video
-
+import numpy
 
 
 # Create your views here. a modo de prueba agregue modelo de videos debo arreglar
 
 
 def sinc(request, num):
-	video = Video.objects.filter(conferencia_id=num)
-	ppt = Diapositiva.objects.filter(conferencia_id=num)
-	if ppt.exists():
-		slide = slides_ppt.objects.filter(id_diapo=ppt)
-		if slide.exists():
-			return render(request, 'sincronizar.html', {'conf' :video, 'slide': slide})
-		return render(request, 'sincronizar.html', {'conf' :video, 'slide': slide})
-	return redirect('app.conferencias_campusd.views.index')	
+    video = Video.objects.filter(conferencia_id=num)
+    ppt = Diapositiva.objects.filter(conferencia_id=num)
+    if ppt.exists():
+        slides = slides_ppt.objects.filter(id_diapo=ppt)
+        if slides.exists():
+            tiempos=numpy.zeros((len(slides),3), dtype=int)
+            tiempos[0,0]=0
+            tiempos[0,1]=round(float(slides[0].duracion),0)
+            tiempos[0,2]=round(float(slides[0].duracion),0)
+            items=[]
+            items.append([slides[0].slide.url,tiempos[0]])
+            for i in range(1,len(slides)):
+                tiempos[i,0]=tiempos[i-1,1]
+                tiempos[i,1]=tiempos[i,0]+round(float(slides[i].duracion),0)
+                tiempos[i,2]=round(float(slides[i].duracion),0)
+                items.append([slides[i].slide.url,tiempos[i]])
+            return render(request, 'sincronizar.html', {'conf' :video, 'items': items})
+        return render(request, 'sincronizar.html', {'conf' :video, 'slide': slides})
+    return redirect('app.conferencias_campusd.views.index')	
 
 
 
@@ -25,8 +36,19 @@ def conferencias(request,num):
     video = Video.objects.filter(conferencia_id=num)
     ppt = Diapositiva.objects.filter(conferencia_id=num)
     if ppt.exists():
-        slide = slides_ppt.objects.filter(id_diapo=ppt)
-        if slide.exists():
-    		return render(request, 'mis_conferencias.html', {'conf' :video, 'slide': slide})
-    	return render(request, 'mis_conferencias.html', {'conf' :video, 'slide': slide})
+        slides = slides_ppt.objects.filter(id_diapo=ppt)
+        if slides.exists():
+            tiempos=numpy.zeros((len(slides),3), dtype=int)
+            tiempos[0,0]=0
+            tiempos[0,1]=round(float(slides[0].duracion),0)
+            tiempos[0,2]=round(float(slides[0].duracion),0)
+            items=[]
+            items.append([slides[0].slide.url,tiempos[0]])
+            for i in range(1,len(slides)):
+                tiempos[i,0]=tiempos[i-1,1]
+                tiempos[i,1]=tiempos[i,0]+round(float(slides[i].duracion),0)
+                tiempos[i,2]=round(float(slides[i].duracion),0)
+                items.append([slides[i].slide.url,tiempos[i]])
+            return render(request, 'mis_conferencias.html', {'conf' :video, 'items': items, 'slide': slides})
+        return render(request, 'mis_conferencias.html', {'conf' :video, 'slide': slides})
     return redirect('app.conferencias_campusd.views.index')	
